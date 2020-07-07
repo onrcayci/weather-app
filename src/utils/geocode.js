d5ae15f5a70f12357e61b7f3e3825a869880b63c
Onur Cayci <onur.cayci@mail.mcgill.ca>
2020-07-07 17:00:18 -0400

    fix comment

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,6 +1,6 @@
 const request = require('postman-request')
 
-//instead of using string address itself, we use encodeURIComponent() to convert the string to the correct form
+// instead of using string address itself, we use encodeURIComponent() to convert the string to the correct form
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1Ijoib2NheWNpIiwiYSI6ImNrYXNoYm9rdTB3d2QycW14cDJzbXhmaDEifQ.9Zo_RJADCV4K9N7BQHNGYQ'
     request({ url, json: true }, (error, { body }) => {
